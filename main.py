b4d37849c88fbfe0fd481d43fb1f63b66a22c45d
Thibaud-Mathis <thibaud.mathis01@gmail.com>
2023-11-07 01:17:32 +0100

    tweeks

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from random import randint
 # ❌ 💥 🌊 🚢
 # https://github.com/Thibaud-Mathis/BattleShip
-# ["1A", "1B ", ... x8], [" ", " ", ... x8], ...x8
+
+ROWS_AND_COLUMNS_NUMBER = 6
+TURNS = 50
+
 #Boards for holding ship locations
-AI_HIDDEN_BOARD = [["  "] * 8 for x in range(8)]
-PLAYER_HIDDEN_BOARD = [["  "] * 8 for x in range(8)]
+# ["1A", "1B ", ... x8], [" ", " ", ... x8], ...x8
+AI_HIDDEN_BOARD = [["  "] * ROWS_AND_COLUMNS_NUMBER for x in range(ROWS_AND_COLUMNS_NUMBER)]
+PLAYER_HIDDEN_BOARD = [["  "] * ROWS_AND_COLUMNS_NUMBER for x in range(ROWS_AND_COLUMNS_NUMBER)]
 # Boards for displaying hits and misses
-AI_GUESS_BOARD = [["🌊"] * 8 for i in range(8)]
-PLAYER_GUESS_BOARD = [["🌊"] * 8 for i in range(8)]
+AI_GUESS_BOARD = [["🌊"] * ROWS_AND_COLUMNS_NUMBER for i in range(ROWS_AND_COLUMNS_NUMBER)]
+PLAYER_GUESS_BOARD = [["🌊"] * ROWS_AND_COLUMNS_NUMBER for i in range(ROWS_AND_COLUMNS_NUMBER)]
+
 
 def print_board(board):
-    print("   A   B   C   D   E   F   G   H")
+    print("   A   B   C   D   E   F")
     row_number = 1
     for row in board:
         print("%d|%s |" % (row_number, " |".join(row)))
@@ -23,8 +28,6 @@ letters_to_numbers = {
     'D': 3,
     'E': 4,
     'F': 5,
-    'G': 6,
-    'H': 7
 }
 numbers_to_letters = {
     0 : 'A',
@@ -33,24 +36,22 @@ numbers_to_letters = {
     3 : 'D',
     4 : 'E',
     5 : 'F',
-    6 : 'G',
-    7 : 'H'
 }
 #computer create 5 ships
 def create_ships(board):
     for ship in range(5):
-        ship_row, ship_column = randint(0,7), randint(0,7)
+        ship_row, ship_column = randint(0,ROWS_AND_COLUMNS_NUMBER - 1), randint(0,ROWS_AND_COLUMNS_NUMBER - 1)
         while board[ship_row][ship_column] == "🚢":
-            ship_row, ship_column = get_user_guess()
+            ship_row, ship_column = randint(0,ROWS_AND_COLUMNS_NUMBER - 1), randint(0,ROWS_AND_COLUMNS_NUMBER - 1)
         board[ship_row][ship_column] = "🚢"
 
 def get_user_guess():
     row = input("Enter the row of the ship: ").upper()
-    while row not in "12345678":
+    while row not in "123456":
         print('Not an appropriate choice, please select a valid row')
         row = input("Enter the row of the ship: ").upper()
     column = input("Enter the column of the ship: ").upper()
-    while column not in "ABCDEFGH":
+    while column not in "ABCDEF":
         print('Not an appropriate choice, please select a valid column')
         column = input("Enter the column of the ship: ").upper()
     return int(row) - 1, letters_to_numbers[column]
@@ -68,10 +69,10 @@ def dispay_interface():
     print('Boats left: ' + str(5 - count_hit_ships(AI_HIDDEN_BOARD)))
     print("You have " + str(turns) + " bullets left")
     print('Guess a battleship location')
-    print("--------- 🤖 AI board 🤖 ----------")
+    print("----- 🤖 AI board 🤖 ------")
     print_board(AI_GUESS_BOARD)
-    print("====================================")
-    print("-------- 👑 Your board 👑 ---------")
+    print("============================")
+    print("---- 👑 Your board 👑 -----")
     print_board(PLAYER_HIDDEN_BOARD)
      
 def handle_turn(guessBoard, hiddenBoard, row, column):
@@ -90,25 +91,22 @@ def handle_turn(guessBoard, hiddenBoard, row, column):
 if __name__ == "__main__":
     create_ships(PLAYER_HIDDEN_BOARD)
     create_ships(AI_HIDDEN_BOARD)
-    turns = 50
+    turns = TURNS
     while turns > 0:
         dispay_interface()
-        # Player turn
         print("==== YOUR TURN ====")
         row, column = get_user_guess()
         alreadyTried = handle_turn(AI_GUESS_BOARD, AI_HIDDEN_BOARD, row, column)
-        # IA turn
+        if count_hit_ships(AI_GUESS_BOARD) == 5:
+            print("🎉 You win ! 🎉")
+            break
         print("==== AI TURN ====")
-        ai_row, ai_column = randint(0,7), randint(0,7)
+        ai_row, ai_column = randint(0, ROWS_AND_COLUMNS_NUMBER - 1), randint(0, ROWS_AND_COLUMNS_NUMBER - 1)
         print("AI guessed " + str(ai_row + 1) + numbers_to_letters[ai_column])
         aiAlreadyTried = handle_turn(PLAYER_GUESS_BOARD, PLAYER_HIDDEN_BOARD, ai_row, ai_column)
         if not alreadyTried or not aiAlreadyTried : turns -= 1
-
-        if count_hit_ships(AI_GUESS_BOARD) == 5:
-            print("You win!")
-            break
         if count_hit_ships(PLAYER_GUESS_BOARD) == 5:
-            print("You lost!")
+            print("☠️ You lost ! ☠️")
             break
         if turns == 0:
-            print("You lost !")
+            print("☠️ You lost ! ☠️")
